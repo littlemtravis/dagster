d489f81c29d63bc1c696dc64898cea67b468d858
prha <prha@elementl.com>
2022-04-28 17:07:37 -0700

    pylint; black

--- examples/docs_snippets/docs_snippets_tests/deploying_tests/dagster_instance_tests/snapshots/snap_test_dagster_instance.py
+++ examples/docs_snippets/docs_snippets_tests/deploying_tests/dagster_instance_tests/snapshots/snap_test_dagster_instance.py
@@ -4,16 +4,15 @@ from __future__ import unicode_literals
 
 from snapshottest import Snapshot
 
-
 snapshots = Snapshot()
 
-snapshots['test_instance_yaml 1'] = [
-    'code_servers',
-    'compute_logs',
-    'local_artifact_storage',
-    'run_coordinator',
-    'run_launcher',
-    'run_monitoring',
-    'storage',
-    'telemetry'
+snapshots["test_instance_yaml 1"] = [
+    "code_servers",
+    "compute_logs",
+    "local_artifact_storage",
+    "run_coordinator",
+    "run_launcher",
+    "run_monitoring",
+    "storage",
+    "telemetry",
 ]
